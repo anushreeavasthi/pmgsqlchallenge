c9327c97ca9cad8113d60785d016075e196bc30c
anushreeavasthi <33330562+anushreeavasthi@users.noreply.github.com>
2022-03-10 22:25:19 -0500

    Update PMG.sql

--- PMG.sql
+++ PMG.sql
@@ -8,6 +8,21 @@ FROM  marketing_data as m
 LEFT JOIN  store_revenue as s
 ON s.date = m.date AND SUBSTRING_INDEX(s.store_location,"-",-1)= m.geo
 GROUP BY m.date, m.geo;
+
+#ALTERNATE QUERY FOR Q3- USING UNION TO DISPLAY MISSING DATA
+
+SELECT m.date as Date, m.geo as Geo, m.impressions, m.clicks, SUM(s.revenue) as Total_Revenue
+FROM  marketing_data as m
+LEFT JOIN  store_revenue as s
+ON s.date = m.date AND SUBSTRING_INDEX(s.store_location,"-",-1)= m.geo
+GROUP BY m.date, m.geo
+union
+SELECT s.date as Date, SUBSTRING_INDEX(s.store_location,"-",-1) as Geo, m.impressions, m.clicks, SUM(s.revenue) as Total_Revenue
+FROM  store_revenue as s
+LEFT JOIN  marketing_data as m
+ON m.date = s.date AND  m.geo=SUBSTRING_INDEX(s.store_location,"-",-1)
+GROUP BY m.date, m.geo;
+
 #4
 SELECT m.geo as Geo,  SUM(m.clicks)/SUM(m.impressions) AS Conversion_rate, SUM(s.revenue)
 FROM  marketing_data as m
